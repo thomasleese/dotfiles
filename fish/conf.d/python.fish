8d1b4d2a86b24460adc7acc2d883a942b12394c0
Thomas Leese <thomas@leese.io>
2018-06-06 08:18:50 +0100

    Set python path

--- fish/conf.d/python.fish
+++ fish/conf.d/python.fish
@@ -1,1 +1,1 @@
-set -gx PATH /usr/local/opt/python/libexec/bin /Users/thomasleese/Library/Python/3.6/bin  $PATH
+set -gx PATH /usr/local/opt/python/bin /Users/thomasleese/Library/Python/3.6/bin  $PATH
