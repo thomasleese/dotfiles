ba94520432bc5d36fdabbc9c899a9508a1bdc389
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-06-26 11:12:56 +0100

    Add a setup for pure theme

---
+++ fish/setup.fish
@@ -0,0 +1,7 @@
+#!/usr/local/bin/fish
+
+# Download the installer to `/tmp`
+curl git.io/pure-fish --output /tmp/pure_installer.fish --location --silent
+# Source and trigger the installer
+source /tmp/pure_installer.fish; and install_pure
+
