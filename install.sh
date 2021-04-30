235349e6f1b2a1b927c8963fc3cffa1bd2c26d03
Thomas Leese <thomas@leese.io>
2021-04-30 10:40:20 +0100

    Combine Poetry installation
    Means we get better error handling from `set -eu`.

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@
 
 set -eu
 
+# Brew
 pushd brew
 brew bundle
 popd
 
+# Git
 pushd git
 rm ~/.gitconfig
 ln -s $(pwd)/gitconfig ~/.gitconfig
 popd
 
+# Fish
 pushd fish
 rm -r ~/.config/fish
 mkdir -p ~/.config/fish
@@ -20,10 +23,12 @@ echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 sudo chsh -s /usr/local/bin/fish $USER
 popd
 
+# Atom
 pushd atom
 rm -r ~/.atom
 mkdir -p ~/.atom
 ln -s $(pwd)/config.cson ~/.atom/config.cson
 popd
 
-./poetry/setup.sh
+# Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
