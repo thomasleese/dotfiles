832d18098e1376272164b887df84c94503c693d1
Thomas Leese <thomas@leese.io>
2021-06-05 17:17:38 +0100

    Switch to Jetbrains

--- install.sh
+++ install.sh
@@ -22,19 +22,5 @@ echo '/usr/local/bin/fish' | sudo tee -a /etc/shells
 sudo chsh -s /usr/local/bin/fish $USER
 popd
 
-# Sublime Text
-pushd sublime-text
-rm -rfv ~/Library/Application\ Support/Sublime\ Text/Packages/User
-mkdir -p ~/Library/Application\ Support/Sublime\ Text/Packages
-ln -s $(pwd) ~/Library/Application\ Support/Sublime\ Text/Packages/User
-popd
-
-# Sublime Merge
-pushd sublime-merge
-rm -rfv ~/Library/Application\ Support/Sublime\ Merge/Packages/User
-mkdir -p ~/Library/Application\ Support/Sublime\ Merge/Packages
-ln -s $(pwd) ~/Library/Application\ Support/Sublime\ Merge/Packages/User
-popd
-
 # Poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
